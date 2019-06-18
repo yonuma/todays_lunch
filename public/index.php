e933e8acd81c110121a5073f0243f72bba3143ff
yonuma <7uk10hnum4@gmail.com>
2019-06-18 17:32:09 +0900

    bugfix

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ use LINE\LINEBot\EchoBot\Dependency;
 use LINE\LINEBot\EchoBot\Route;
 use LINE\LINEBot\EchoBot\Setting;
 require_once __DIR__ . '/../vendor/autoload.php';
-$setting = Setting::getSetting();
+$setting = setting::getSetting();
 $app = new Slim\App($setting);
 (new dependencies())->register($app);
 (new routes())->register($app);
