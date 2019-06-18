86bc3f0a90c15530ae6075c88e648fdd9f13713f
yonuma <7uk10hnum4@gmail.com>
2019-06-18 16:54:20 +0900

    エコーボット

--- public/index.php
+++ public/index.php
@@ -1,35 +1,10 @@
 <?php
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-    if (is_file($file)) {
-        return false;
-    }
-}
-
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../controller/IndexController.php';
-require __DIR__ . '/../src/routes.php';
-
-// session_start();
-
-// // Instantiate the app
-// $settings = require __DIR__ . '/../src/settings.php';
-// $app = new \Slim\App($settings);
-
-// // Set up dependencies
-// $dependencies = require __DIR__ . '/../src/dependencies.php';
-// $dependencies($app);
-
-// // Register middleware
-// $middleware = require __DIR__ . '/../src/middleware.php';
-// $middleware($app);
-
-// // Register routes
-// $routes = require __DIR__ . '/../src/routes.php';
-// $routes($app);
-
-// // Run app
-// $app->run();
+use LINE\LINEBot\EchoBot\Dependency;
+use LINE\LINEBot\EchoBot\Route;
+use LINE\LINEBot\EchoBot\Setting;
+require_once __DIR__ . '/../vendor/autoload.php';
+$setting = Setting::getSetting();
+$app = new Slim\App($setting);
+(new Dependency())->register($app);
+(new Route())->register($app);
+$app->run();
--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,47 @@
 <?php
-
-use Slim\App;
-use Slim\Http\Request;
-use Slim\Http\Response;
-use \GuzzleHttp\Client;
-
-
-    $app = new \Slim\App;
-    $app->post('/', IndexController::class . ':index');
-    $app->run();
-
-
+namespace LINE\LINEBot\EchoBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Exception\InvalidEventRequestException;
+use LINE\LINEBot\Exception\InvalidSignatureException;
+class Route
+{
+    public function register(\Slim\App $app)
+    {
+        $app->post('/callback', function (\Slim\Http\Request $req, \Slim\Http\Response $res) {
+            /** @var \LINE\LINEBot $bot */
+            $bot = $this->bot;
+            /** @var \Monolog\Logger $logger */
+            $logger = $this->logger;
+            $signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
+            if (empty($signature)) {
+                return $res->withStatus(400, 'Bad Request');
+            }
+            // Check request with signature and parse request
+            try {
+                $events = $bot->parseEventRequest($req->getBody(), $signature[0]);
+            } catch (InvalidSignatureException $e) {
+                return $res->withStatus(400, 'Invalid signature');
+            } catch (InvalidEventRequestException $e) {
+                return $res->withStatus(400, "Invalid event request");
+            }
+            foreach ($events as $event) {
+                if (!($event instanceof MessageEvent)) {
+                    $logger->info('Non message event has come');
+                    continue;
+                }
+                if (!($event instanceof TextMessage)) {
+                    $logger->info('Non text message has come');
+                    continue;
+                }
+                $replyText = $event->getText();
+                $logger->info('Reply text: ' . $replyText);
+                $resp = $bot->replyText($event->getReplyToken(), $replyText);
+                $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+            }
+            $res->write('OK');
+            return $res;
+        });
+    }
+}
