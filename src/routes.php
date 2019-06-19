99fcbe2bb66d513eb898155ebab12743e431a0fc
yonuma <7uk10hnum4@gmail.com>
2019-06-19 16:52:07 +0900

    lunch serch

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,18 @@
 <?php
 namespace EchoBot;
+use \GuzzleHttp\Client;
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\Event\MessageEvent;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Event\MessageEvent\LocationMessage;
 use LINE\LINEBot\Exception\InvalidEventRequestException;
 use LINE\LINEBot\Exception\InvalidSignatureException;
 class Route
 {
+    const RANGE = 2;
+    const LUNCH = 1;
+    const CARD  = 1;
+    const HIT   = 10;
     public function register(\Slim\App $app)
     {
         $app->post('/callback', function (\Slim\Http\Request $req, \Slim\Http\Response $res) {
@@ -31,17 +37,38 @@ class Route
                     $logger->info('Non message event has come');
                     continue;
                 }
-                if (!($event instanceof TextMessage)) {
-                    $logger->info('Non text message has come');
+                if (!($event instanceof LocationMessage)) {
+                    $logger->info('Non location message has come');
                     continue;
                 }
-                $replyText = $event->getText();
-                $logger->info('Reply text: ' . $replyText);
+                $latitude = $event->getLatitude();
+                $longitude = $event->getLongitude();
+                $logger->info('Reply text: ' . $latitude . ':' . $longitude);
+                $lunchData = getLunch($latitude, $longitude);
+                foreach($lunchData->rest as $storeData){
+                    $replyText .= $storeData->name;
+                }
                 $resp = $bot->replyText($event->getReplyToken(), $replyText);
                 $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
             }
             $res->write('OK');
             return $res;
         });
+    }
+
+    public function getLunch($latitude, $longitude)
+    {
+        $client = new Client([
+            'base_uri' => 'https://api.gnavi.co.jp/RestSearchAPI/v3/',
+        ]);
+
+        $method = 'GET';
+        $access_key = getenv('GURUNAVI_ACCESS_KEY');
+        $uri = '?keyid='.$access_key.'&latitude='.$latitude.'&longitude='.$longitude.'&range='.self::RANGE.'&lunch='.self::LUNCH.'&card='.self::CARD.'&hit_per_page='.self::HIT;
+        $options = [];
+        $response = $client->request($method, $uri, $options);
+        $list = json_decode($response->getBody()->getContents(), true);
+
+        return $this->response($list);
     }
 }
